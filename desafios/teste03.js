b972bfa993c8f124f5239bf2504a2d37cb40a9e9
amorim <>
2023-08-11 22:42:50 -0300

    up

---
+++ desafios/teste03.js
@@ -0,0 +1,9 @@
+var salario = 4000
+console.log(`Se você recebe ${salario} você é:`)
+if(salario >= 4000){
+    console.log('Rico')
+} else if(salario <= 2000) {
+    console.log('Pobre')
+} else{
+    console.log('Classe média')
+}
